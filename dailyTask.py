24326389e22028ea8ff6403352fa4e58567e82f6
justinhoejj <hoejj05@gmail.com>
2021-12-27 15:58:59 +0800

    add notification should daily task fail

--- dailyTask.py
+++ dailyTask.py
@@ -1,14 +1,24 @@
 from dataManager import get_crypto_symbols ,update_price_data
 from subscription import simulate_notify
+from notification import send_mail
 
 def daily_task():
   for crypto_symbol in get_crypto_symbols():
     try:
       # Update price data
       update_price_data(crypto_symbol)
+    except Exception as e:
+      send_mail('hoejj05@gmail.com', 
+      f'Failed to update price for {crypto_symbol}',
+      e)
+      continue
+    try:
       # Notify subscribers
       simulate_notify(crypto_symbol)
-    except Exception:
+    except Exception as e:
+      send_mail('hoejj05@gmail.com', 
+      f'Failed to simulate and notifiy for {crypto_symbol}',
+      e)
       continue
     
 
